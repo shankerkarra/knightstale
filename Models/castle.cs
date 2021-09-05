e54c802392f3cff0f1a61a8d0422d151690ba815
Shanker Karra <shankerkarra@gmail.com>
2021-09-05 08:37:00 -0600

    checkin knightstale

---
+++ Models/castle.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace knightstale.Models
+{
+  public class castle
+  {
+    public int id { get; set; }
+    public string name { get; set; }
+    public string location { get; set; }
+  }
+}
